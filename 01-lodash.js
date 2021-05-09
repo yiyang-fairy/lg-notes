be686e2676ff1e794906a0d1369694205bfd5028
yiyang-fairy <1196342044@qq.com>
2021-05-09 10:33:26 +0800

    第一次提交

---
+++ 01-lodash.js
@@ -0,0 +1,8 @@
+//first / last /toUpper / reverse / each / includes / find / findIndex
+
+const _ = require('lodash')
+
+const array = ['one', 'two', 'three', 'four']
+
+console.log(_.first(array))
+console.log(_.last(array))
